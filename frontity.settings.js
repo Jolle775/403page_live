03354f5e6fd7404b4cb6bd61c2209cdffa7fef4f
403pagelabs <60448695+403pagelabs@users.noreply.github.com>
2020-01-29 21:25:42 +0000

    Initial commit

---
+++ frontity.settings.js
@@ -0,0 +1,54 @@
+const settings = {
+  "name": "403page2020",
+  "state": {
+    "frontity": {
+      "url": "https://403page.com",
+      "title": "Indestructible Uptime | 403Page.com",
+      "description": "Blazing-edge Wordpress development"
+    }
+  },
+  "packages": [
+    {
+      "name": "fourothree",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "All",
+              "/"
+            ],
+            [
+              "Performance",
+              "/category/performance/"
+            ],
+            [
+              "Errors",
+              "/category/errors/"
+            ],
+            [
+              "How to...",
+              "/category/how-to/"
+            ],
+
+          ],
+          "featured": {
+            "showOnList": true,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://restfeed.403page.com/wp-json"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
